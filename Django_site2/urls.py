d2c7acf0c19962482a429af45bc743b36fc0c923
xaxaxa2283-ctrl <xaxaxa2283@gmail.com>
2026-06-14 21:57:00 +0500

    Redirect legacy privacy link to current policy

--- Django_site2/urls.py
+++ Django_site2/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 from sitemaps import ProductSitemap, StaticViewSitemap
 
@@ -14,6 +15,7 @@ sitemaps = {
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("legal/", include("legal.urls", namespace="legal")),
+    path("privacy/", RedirectView.as_view(url="/legal/privacy/", permanent=False)),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
     path("", include("main.urls", namespace="main")),
 ]
