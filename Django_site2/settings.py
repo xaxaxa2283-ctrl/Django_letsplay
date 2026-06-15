b4316c6146d127fe889bae6d752cc6489c9f69db
xaxaxa2283-ctrl <xaxaxa2283@gmail.com>
2026-06-15 12:16:43 +0500

    Разрешить хранение SQLite вне каталога приложения

--- Django_site2/settings.py
+++ Django_site2/settings.py
@@ -68,7 +68,12 @@ TEMPLATES = [{
 }]
 WSGI_APPLICATION = "Django_site2.wsgi.application"
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": Path(os.getenv("SQLITE_DATABASE_PATH", BASE_DIR / "db.sqlite3")),
+    }
+}
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
